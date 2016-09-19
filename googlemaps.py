82b629e9df57e539f774a9be99d29c683ed25797
LeviBarnes <levi.barnes@gmail.com>
2016-09-19 13:08:31 -0700

    Catching more errors.

--- googlemaps.py
+++ googlemaps.py
@@ -3,6 +3,12 @@ import urllib2
 
 from api_key import my_key
 
+if my_key=="Not a real key. Get yours at https://developers.google.com/maps/documentation/distance-matrix/get-api-key, then put it here." :
+   print "FAIL! You need your own key from "
+   print "   https://developers.google.com/maps/documentation/distance-matrix/get-api-key"
+   print " Get a key, modify api_key.py to assign my_key. Then try again."
+   exit()
+
 start = raw_input("Enter origin city: ")
 end = raw_input("Enter destination city: ")
 
@@ -18,11 +24,14 @@ resp=urllib2.urlopen(url).read()
 output = json.loads(resp)
 #print output
 if output['status'] != 'OK':
-   print "FAIL!"
+   print "FAIL! No response from Google Maps API. Is that a real key?"
 else:
-   print "It takes "+ output['rows'][0]['elements'][0]['duration']['text'] + \
-       " to get from " + \
-       output['origin_addresses'][0] + " to " + \
-       output['destination_addresses'][0] 
+   try:
+      print "It takes "+ output['rows'][0]['elements'][0]['duration']['text'] + \
+          " to get from " + \
+          output['origin_addresses'][0] + " to " + \
+          output['destination_addresses'][0] 
+   except:
+      print "FAIL! Bad output from API. Are those real places?"
 
 
